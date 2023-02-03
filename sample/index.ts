0861f97ec687e82a62345fdc5b25d8bf5ec40f77
pradeep-selva <pradeepselvaraju@gmail.com>
2023-02-03 19:14:18 +0530

    ft: sample plan compilation

--- sample/index.ts
+++ sample/index.ts
@@ -2,8 +2,17 @@ import {BrahVM} from "brah-weiroll-sdk";
 
 const main = () => {
   const vm = new BrahVM();
+  const vault = vm.contractByName("Vault");
 
-  console.log(vm.planner);
+  vm.planner.add(
+    vault.functions.withdraw(
+      1000,
+      "0xE9F47d5EE5b73326e1EB9361630105e8Ca386874",
+    ),
+  );
+  vm.assert(vault.functions.totalVaultFunds());
+
+  vm.compile(true);
 };
 
 main();
